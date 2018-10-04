098cfdc7d5b6d7e65e3c5b7624bb1caca4ef8637
inuscg <>
2018-10-04 12:57:08 +0530

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {
     host: 'task-manager-inuscg.herokuapp.com'
   }
-  # config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = {
   #   :address => "smtp.gmail.com",
   #   :port => 587,
@@ -109,7 +109,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :user_name => 'pooja-inuscg',
     :password => 'data12care',
-    :domain => 'task-manager-inuscg.herokuapp.com',
+    :domain => 'herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
