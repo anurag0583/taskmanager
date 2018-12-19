9cdccdfd007fef1fb8b7ddee2a184a3e73e93c6c
inuscg <anurag0583@gmial.com>
2018-12-19 14:04:26 +0530

    chnages

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,11 +91,11 @@ Rails.application.configure do
 
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.asset_host = "task-manager-inuscg.herokuapp.com"
-  config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.default_url_options = {
     host: 'task-manager-inuscg.herokuapp.com'
   }
-  # config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = {
   #   :address => "smtp.gmail.com",
   #   :port => 587,
