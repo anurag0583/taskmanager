7c1eddb81e46d4eb67cfceb50a4a7eeb6abcdde1
inuscg <anurag0583@gmial.com>
2018-12-19 14:01:29 +0530

    chnages

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,12 +54,12 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker 
   config.action_mailer.asset_host = "http://localhost:3000"
-  config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.default_url_options = {
     host: 'localhost',
     port: 3000
   }
-  # config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = {
   #   :address => "smtp.gmail.com",
   #   :port => 587,
@@ -70,15 +70,15 @@ Rails.application.configure do
   #   password: 'pooja#12345'
   # }
  
-  # ActionMailer::Base.smtp_settings = {
-  #   :user_name => 'pooja-inuscg',
-  #   :password => 'data12care',
-  #   :domain => 'localhost:3000',
-  #   :address => 'smtp.sendgrid.net',
-  #   :port => 587,
-  #   :authentication => :plain,
-  #   :enable_starttls_auto => true
-  # }
+  ActionMailer::Base.smtp_settings = {
+    :user_name => 'pooja-inus',
+    :password => 'D@ata12care',
+    :domain => 'localhost:3000',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
   
   # config.consider_all_requests_local = false
 
